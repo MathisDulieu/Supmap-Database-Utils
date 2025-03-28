3d4516368b3cc36dc49edaf347ba83c0bb052dfe
Mathis <106063734+MathisDulieu@users.noreply.github.com>
2025-03-28 21:56:25 +0100

    First commit

---
+++ src/main/java/com/novus/database_utils/DatabaseUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.novus.database_utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseUtilsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novus/database_utils/DatabaseUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novus.database_utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
