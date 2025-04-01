d1b00845150187d0e0d366981acabf6d3d17be57
Mathis <106063734+MathisDulieu@users.noreply.github.com>
2025-04-01 15:03:53 +0200

    [/] UserDao

--- src/main/java/com/novus/database_utils/User/UserDao.java
+++ src/main/java/com/novus/database_utils/User/UserDao.java
@@ -2,6 +2,8 @@ package com.novus.database_utils.User;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -24,6 +26,18 @@ public class UserDao<T> {
 
     public void delete(T entity) {
         mongoTemplate.remove(entity, USER_COLLECTION);
+    }
+
+    public boolean isUsernameAlreadyUsed(String username) {
+        return mongoTemplate.exists(new Query(Criteria.where("username").is(username)), USER_COLLECTION);
+    }
+
+    public boolean isEmailAlreadyUsed(String email) {
+        return mongoTemplate.exists(new Query(Criteria.where("email").is(email)), USER_COLLECTION);
+    }
+
+    public Optional<T> findByEmail(String email, Class<T> entityClass) {
+        return Optional.of(mongoTemplate.findOne(new Query(Criteria.where("email").is(email)), entityClass, USER_COLLECTION));
     }
 
 }
