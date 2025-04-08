483c1281b8020c746c3f8fefd7b3b445ce5fc925
Mathis <106063734+MathisDulieu@users.noreply.github.com>
2025-04-08 13:31:09 +0200

    [/] shared models version

---
+++ src/main/java/com/novus/database_utils/Log/LogDao.java
@@ -0,0 +1,21 @@
+package com.novus.database_utils.Log;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class LogDao<T> {
+
+    private final MongoTemplate mongoTemplate;
+
+    private static final String LOG_COLLECTION = "LOGS";
+
+    public void save(T entity) {
+        mongoTemplate.save(entity, LOG_COLLECTION);
+    }
+
+}
